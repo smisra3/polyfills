88a652d1a54aaa89074027fb5c6325613da2dc87
Shubhankar Misra <shumisra@publicisgroupe.net>
2018-10-10 18:44:58 +0530

    Adding the error handling

--- PromiseAll_polyfill.js
+++ PromiseAll_polyfill.js
@@ -1,13 +1,12 @@
-
-
 /**
  * NOTE----------- THis is not sequential execution of promises. Just the order is maintained.
  * This is a polyfill for Promises.all which works in exactly the same manner as it's real brother.
  * @param {Array} arrayOfPromises Is the array of promises passed to this method which will return a new promise
  * @return {Promise} Which can be utilised and the reponse will be in the same order in an array in which the promises were index in the passed array.
  */
-
 function PromiseAll(arrayOfPromises) {
+  if (Object.prototype.toString.call(arrayOfPromises) !== '[object Array]')
+    return false;
   return new Promise(function (resolve, reject) {
     var resultArray = [];
     if (!arrayOfPromises.length)
