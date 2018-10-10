a722c10ff0b8104c99a8803c20eca7c74edfc9a2
Shubhankar Misra <shumisra@publicisgroupe.net>
2018-10-10 18:41:23 +0530

    Adding the polyfill for Promise.all

---
+++ PromiseAll_polyfill.js
@@ -0,0 +1,28 @@
+
+
+/**
+ * NOTE----------- THis is not sequential execution of promises. Just the order is maintained.
+ * This is a polyfill for Promises.all which works in exactly the same manner as it's real brother.
+ * @param {Array} arrayOfPromises Is the array of promises passed to this method which will return a new promise
+ * @return {Promise} Which can be utilised and the reponse will be in the same order in an array in which the promises were index in the passed array.
+ */
+
+function PromiseAll(arrayOfPromises) {
+  return new Promise(function (resolve, reject) {
+    var resultArray = [];
+    if (!arrayOfPromises.length)
+      resolve(arrayOfPromises);
+    var pendingPromises = arrayOfPromises.length;
+    arrayOfPromises.forEach(function (promise, index) {
+      promise.then(function (result) {
+        resultArray[index] = result;
+        pendingPromises--;
+        if (pendingPromises === 0) {
+          resolve(resultArray);
+        }
+      }, function (error) {
+        reject(error);
+      });
+    });
+  });
+}
