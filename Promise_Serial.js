344cee00f711586e0286603ba964755062c67e4a
Shubhankar Misra <shubhankarmisra.bit@gmail.com>
2018-10-12 21:16:36 +0530

    Adding the documentation for PromiseSerial

--- Promise_Serial.js
+++ Promise_Serial.js
@@ -1,9 +1,20 @@
-const urls = ['/url1', '/url2', '/url3'];
-
-const funcs = urls.map( url => () => fetch(url));
+/**
+ * PrmoiseSerial is a function that accepts an array of functions each having a promise returned.
+ * We call them according to our will to solve them sequentially.
+ * @param { Array } funcs The array of functions passed to this method, where each function returns a Promise.
+ * @return { Array } The Array containing resolved promises as a one Promise which is thenable.
+ */
 
 const PromiseSerial = funcs => {
   funcs.reduce((promise, func) => {
     promise.then( result => func().then( data => result.push(data)))
   }, Promise.resolve([]))
 };
+
+// Usage
+
+const urls = ['/url1', '/url2', '/url3'];
+
+const funcs = urls.map( url => () => fetch(url));
+
+PromiseSerial(funcs).then( resultArray => console.log(resultArray)).catch( error => new Error());
